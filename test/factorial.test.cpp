e9a9c4ab30a095cd26a1dace60b4581410b9d937
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-26 20:55:45 -0300

    test: add tests to factorial_naive and factorial_memoization

--- test/factorial.test.cpp
+++ test/factorial.test.cpp
@@ -2,9 +2,24 @@
 
 #include "factorial.hpp"
 
-TEST_CASE( "compute factorials", "[factorial]" ) {
-    REQUIRE( factorial(1) == 1 );
-    REQUIRE( factorial(2) == 2 );
-    REQUIRE( factorial(3) == 6 );
-    REQUIRE( factorial(10) == 3628800 );
+TEST_CASE( "compute factorials - naive", "[factorial]" ) {
+    REQUIRE(factorial_naive(1) == 1);
+    REQUIRE(factorial_naive(2) == 2);
+    REQUIRE(factorial_naive(3) == 6);
+    REQUIRE(factorial_naive(10) == 3628800);
 }
+
+TEST_CASE( "compute factorials - memoization", "[factorial]" ) {
+    REQUIRE(factorial_memoization(1) == 1);
+    REQUIRE(factorial_memoization(2) == 2);
+    REQUIRE(factorial_memoization(3) == 6);
+    REQUIRE(factorial_memoization(10) == 3628800);
+}
+
+TEST_CASE( "compute factorials", "[factorial]" ) {
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3628800);
+}
+
