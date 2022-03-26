c2c7152803544a25713c97a09f3050953ef6f436
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-26 18:18:55 -0300

    feat: add fibonacci implementation
    That specific implementation uses memoization.

---
+++ src/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include "fibonacci.hpp"
+
+#include <initializer_list>
+#include <vector>
+
+constexpr auto initialFibonacciValues = {0lu, 1lu};
+
+long unsigned fibonacci (const long unsigned n) {
+    static std::vector<long unsigned> values(initialFibonacciValues);
+    if (n < values.size()) return values[n];
+    else {
+        const long int result = fibonacci(n-1) + fibonacci(n-2);
+        values.push_back(result);
+        return result;
+    }
+}
