833ed7d572c8eeffa124e9b37cab71cd1ba5b4ed
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-03-26 21:54:09 -0300

    test: update factorial tests

--- test/factorial.test.cpp
+++ test/factorial.test.cpp
@@ -1,25 +1,30 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "factorial.hpp"
+#include <iostream>
 
 TEST_CASE( "compute factorials - naive", "[factorial]" ) {
-    REQUIRE(factorial_naive(1) == 1);
-    REQUIRE(factorial_naive(2) == 2);
-    REQUIRE(factorial_naive(3) == 6);
-    REQUIRE(factorial_naive(10) == 3628800);
+    REQUIRE(factorial_naive( 1) == 1lu);
+    REQUIRE(factorial_naive( 2) == 2lu);
+    REQUIRE(factorial_naive( 3) == 6lu);
+    REQUIRE(factorial_naive(10) == 3628800lu);
+    REQUIRE(factorial_naive(20) == 2432902008176640000lu);
 }
 
 TEST_CASE( "compute factorials - memoization", "[factorial]" ) {
-    REQUIRE(factorial_memoization(1) == 1);
-    REQUIRE(factorial_memoization(2) == 2);
-    REQUIRE(factorial_memoization(3) == 6);
-    REQUIRE(factorial_memoization(10) == 3628800);
+    REQUIRE(factorial_memoization( 1) == 1lu);
+    REQUIRE(factorial_memoization( 2) == 2lu);
+    REQUIRE(factorial_memoization( 3) == 6lu);
+    REQUIRE(factorial_memoization(10) == 3628800lu);
+    REQUIRE(factorial_memoization(20) == 2432902008176640000lu);
 }
 
 TEST_CASE( "compute factorials", "[factorial]" ) {
-    REQUIRE(factorial(1) == 1);
-    REQUIRE(factorial(2) == 2);
-    REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(10) == 3628800);
+    REQUIRE(factorial( 1) == 1lu);
+    REQUIRE(factorial( 2) == 2lu);
+    REQUIRE(factorial( 3) == 6lu);
+    REQUIRE(factorial(10) == 3628800lu);
+    REQUIRE(factorial(20) == 2432902008176640000lu);
+    REQUIRE_THROWS(factorial(21));
 }
 
