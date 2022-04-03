e51a26972f3cc6fc87636b6038d215b85be2356d
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-04-03 09:52:58 -0300

    test(on the way home): add tests of all variants

--- test/onTheWayHome.test.cpp
+++ test/onTheWayHome.test.cpp
@@ -45,7 +45,7 @@ TEST_CASE("solution", "[onTheWayHome]") {
         {{1, 1},   2},
         {{2, 2},   6},
         {{0, 3},   1},
-        {{3, 0},   1},
+        {{3, 0},   1},//this
         {{0, 9},   1},
         {{9, 0},   1},
         {{3, 4},  35},
@@ -59,7 +59,83 @@ TEST_CASE("solution", "[onTheWayHome]") {
     }
 }
 
-TEST_CASE("naive solution", "[onTheWayHome]") {
+TEST_CASE("boundary conditions - optimized solution", "[onTheWayHome]") {
+    SECTION("first row") {
+        const std::vector<TestInstance> testInstances = {
+            {{0, 0                     }, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+        };
+        for (const auto& instance: testInstances) {
+            CHECK(onTheWayHome(instance.position) == instance.numberOfUniquePaths);
+        }
+    }
+    SECTION("first column") {
+        const std::vector<TestInstance> testInstances = {
+            {{0,                      0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+        };
+        for (const auto& instance: testInstances) {
+            CHECK(onTheWayHome_opt(instance.position) == instance.numberOfUniquePaths);
+        }
+    }
+}
+
+TEST_CASE("solution optimized", "[onTheWayHome]") {
+    const std::vector<TestInstance> testInstances = {
+        {{0, 0},   1},
+        {{1, 1},   2},
+        {{2, 2},   6},
+        {{0, 3},   1},
+        {{3, 0},   1},
+        {{0, 9},   1},
+        {{9, 0},   1},
+        {{3, 4},  35},
+        {{5, 5}, 252},
+        {{5, 1},   6},
+        {{1, 5},   6},
+        {{1, 5},   6},
+    };
+    for (const auto& instance: testInstances) {
+        CHECK(onTheWayHome_opt(instance.position) == instance.numberOfUniquePaths);
+    }
+}
+
+TEST_CASE("boundary conditions - naive", "[onTheWayHome]") {
+    SECTION("first row") {
+        const std::vector<TestInstance> testInstances = {
+            {{0, 0                     }, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+            {{0, generateRandomNumber()}, 1},
+        };
+        for (const auto& instance: testInstances) {
+            CHECK(onTheWayHome_naive(instance.position) == instance.numberOfUniquePaths);
+        }
+    }
+    SECTION("first column") {
+        const std::vector<TestInstance> testInstances = {
+            {{0,                      0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+            {{generateRandomNumber(), 0}, 1},
+        };
+        for (const auto& instance: testInstances) {
+            CHECK(onTheWayHome_naive(instance.position) == instance.numberOfUniquePaths);
+        }
+    }
+}
+
+TEST_CASE("solution - naive", "[onTheWayHome]") {
     const std::vector<TestInstance> testInstances = {
         {{0, 0},   1},
         {{0, 9},   1},
