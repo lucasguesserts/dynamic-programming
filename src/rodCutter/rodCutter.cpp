5af1cda35c4860a89ef18fafbffd84e316ef20ed
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-04-07 09:32:32 -0300

    feat(rod cutter): implement algorithm that solver the problem

---
+++ src/rodCutter/rodCutter.cpp
@@ -0,0 +1,51 @@
+#include "rodCutter.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+
+using namespace RodCutter;
+
+Problem::Problem(
+    const std::vector<double>& pricesOfRodPieces
+) {
+    this->checkPrices(pricesOfRodPieces);
+    this->pricesOfRodPieces = pricesOfRodPieces;
+    this->pricesOfRodPieces.insert(this->pricesOfRodPieces.begin(), 0.0); // value at zero
+}
+
+void Problem::checkPrices (const std::vector<double>& prices) {
+    for (const auto& price: prices) {
+        if (price <= 0.0) throw std::runtime_error("The price of the pieces of rod must be positive, but a non-positive value has been provided");
+    }
+}
+
+unsigned Problem::getMaximumRodSize () const {
+    return this->pricesOfRodPieces.size();
+}
+
+double Problem::price (const unsigned& rodSize) const {
+    return this->pricesOfRodPieces[rodSize];
+}
+
+Solver::Solver (const Problem& problem)
+: problem(problem) {}
+
+double Solver::operator() (const unsigned& rodSize) {
+    this->checkProblemSize(rodSize);
+    const unsigned halfSize = rodSize / 2u; // floor(rodSize / 2)
+    const unsigned solutionsToCompute = halfSize + 1u;
+    std::vector<double> bestPrices(solutionsToCompute, 0.0);
+    bestPrices[0] = this->problem.price(rodSize);
+    for (unsigned i = 1; i < solutionsToCompute; ++i) {
+        bestPrices[i] = this->problem.price(i) + this->problem.price(rodSize - i);
+    }
+    const double max = *std::max_element(bestPrices.cbegin(), bestPrices.cend());
+    return max;
+}
+
+void Solver::checkProblemSize (const unsigned& rodSize) {
+    if (rodSize > this->problem.getMaximumRodSize())
+        throw std::runtime_error("problem size is larger than the maximum allowed");
+}
--- test/rodCutter.test.cpp
+++ test/rodCutter.test.cpp
@@ -1,20 +1,19 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
 
 #include <initializer_list>
 
 #include "rodCutter.hpp"
 
+using namespace Catch::literals;
+
 TEST_CASE("rodCutter", "[rodCutter]") {
-    const unsigned rodSize = 4;
-    const std::initializer_list<unsigned> pricesOfRodPieces = {2, 4, 7, 8};
-    const auto problem = RodCutter::ProblemBuilder()
-        .rodSize(rodSize)
-        .prices(pricesOfRodPieces)
-        .build();
+    const std::initializer_list<double> pricesOfRodPieces = {2, 4, 7, 8};
+    const auto problem = RodCutter::Problem(pricesOfRodPieces);
     auto solver = RodCutter::Solver(problem);
-    CHECK(solver(0u) == 0u);
-    CHECK(solver(1u) == 2u);
-    CHECK(solver(2u) == 4u);
-    CHECK(solver(3u) == 7u);
-    CHECK(solver(4u) == 9u);
+    CHECK(solver(0u) == 0.0_a);
+    CHECK(solver(1u) == 2.0_a);
+    CHECK(solver(2u) == 4.0_a);
+    CHECK(solver(3u) == 7.0_a);
+    CHECK(solver(4u) == 9.0_a);
 }
