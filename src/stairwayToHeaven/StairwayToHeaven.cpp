9a919dd05116448a5f87137a659bb6dc87475fa0
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-04-12 17:13:29 -0300

    feat: implement a stairway to heaven solution using an object for memoization

---
+++ src/stairwayToHeaven/StairwayToHeaven.cpp
@@ -0,0 +1,22 @@
+#include "stairwayToHeaven.hpp"
+#include "factorial.hpp"
+
+#include <vector>
+
+constexpr auto initialValues = { 1lu, 1lu, 2lu };
+
+StairwayToHeaven::StairwayToHeaven () {
+    this->values = initialValues;
+}
+
+
+long unsigned StairwayToHeaven::operator() (const long unsigned n) {
+    static std::vector<long unsigned> values = initialValues;
+    if (n < values.size()) {
+        return values[n];
+    } else {
+        const long unsigned result = this->operator()(n - 1) + this->operator()(n - 2);
+        values.push_back(result);
+        return result;
+    }
+}
