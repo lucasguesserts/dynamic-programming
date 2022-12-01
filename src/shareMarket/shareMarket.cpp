732b3e08d031030e146f27d6563cbeb9b1d09ef3
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2022-12-01 07:52:37 -0300

    refactor(share market): improve method implementation

--- src/shareMarket/shareMarket.cpp
+++ src/shareMarket/shareMarket.cpp
@@ -25,31 +25,38 @@ Price computePrice(const Prices & prices, const Solution & solution) {
     return acc;
 }
 
-Price shareMarket(const Prices & prices) {
+Solution initializeSolution(const Prices & prices) {
     Solution solution;
     // initialization: find the first pair
-    for (Index i = 0; i < prices.size() - 1; ++i) {
-        if (prices[i+1] - prices[i] > 0.0) {
-            auto initialOperation = Operation(i, i+1);
+    for (Index index = 0; index < prices.size() - 1; ++index) {
+        const auto nextIndex = index + 1;
+        const auto priceDiff = prices[nextIndex] - prices[index];
+        if (priceDiff > 0.0) {
+            auto initialOperation = Operation(index, nextIndex);
             solution.emplace_back(initialOperation);
             break;
         }
     }
+    return solution;
+}
+
+Price shareMarket(const Prices & prices) {
+    // initialization
+    Solution solution = initializeSolution(prices);
+    // boundary condition
+    if (solution.empty()) {
+        return 0.0;
+    }
     // search
-    if (solution.size() == 0) return 0.0;
-    for (Index i = solution.back().sell + 1; i < prices.size(); ++i) {
-        const Index lastSellIndex = solution.back().sell;
-        const Price lastSellPrice = prices[lastSellIndex];
-        const Price previousSellPrice = prices[i - 1];
-        const Price currentSellPrice = prices[i];
-        if (lastSellIndex == i - 1 && currentSellPrice >= lastSellPrice) {
-            solution.back().sell = i;
-        } else if (lastSellIndex == i - 1 && currentSellPrice < lastSellPrice) {
-            continue;
-        } else if (lastSellIndex < i - 1 && currentSellPrice <= previousSellPrice) {
-            continue;
-        } else if (lastSellIndex < i - 1 && currentSellPrice > previousSellPrice) {
-            solution.emplace_back(Operation(i-1, i));
+    for (Index currentIndex = solution.back().sell + 1; currentIndex < prices.size(); ++currentIndex) {
+        const Index & lastSellIndex = solution.back().sell;
+        const Price & lastSellPrice = prices[lastSellIndex];
+        const Price & previousSellPrice = prices[currentIndex - 1];
+        const Price & currentSellPrice = prices[currentIndex];
+        if ((lastSellIndex == (currentIndex - 1)) && (currentSellPrice >= lastSellPrice)) {
+            solution.back().sell = currentIndex;
+        } else if (currentSellPrice > previousSellPrice) {
+            solution.emplace_back(Operation(currentIndex-1, currentIndex));
         }
     }
     // compute price
