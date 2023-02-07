bf25c9d2c2447ee3c071721be4c608d270f414b9
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-02-07 11:00:10 -0300

    test(longest increasing subsequence): add a simple test for the problem

---
+++ src/longestIncreasingSubsequence.hpp
@@ -0,0 +1,8 @@
+#ifndef LONGEST_INCREASING_SUBSEQUENCE_HPP_
+#define LONGEST_INCREASING_SUBSEQUENCE_HPP_
+
+#include <vector>
+
+unsigned longestIncreasingSubsequence(const std::vector<double> & values);
+
+#endif
---
+++ src/longestIncreasingSubsequence/longestIncreasingSubsequence.cpp
@@ -0,0 +1,5 @@
+#include "longestIncreasingSubsequence.hpp"
+
+unsigned longestIncreasingSubsequence([[maybe_unused]] const std::vector<double> & values) {
+    return 0u;
+}
