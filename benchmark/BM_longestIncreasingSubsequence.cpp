5d301576b1b2a9589a73ffbe4602f7942873cbfa
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-02-07 22:09:22 -0300

    test(benchmark): add benchmark for naive algorithm for longest increasing subsequence

--- benchmark/BM_longestIncreasingSubsequence.cpp
+++ benchmark/BM_longestIncreasingSubsequence.cpp
@@ -4,11 +4,21 @@
 
 const LongestIncreasingSubsequence::Sequence sequence = {10, 3, 9, 4, 7, 1, 9};
 
-static void BM_subsequenceGenerator(benchmark::State& state) {
+static void BM_longestIncreasingSubsequence_subsequenceGenerator(benchmark::State& state) {
   for (auto _ : state) {
       const LongestIncreasingSubsequence::SubsequenceGenerator gen(sequence);
   }
 }
-BENCHMARK(BM_subsequenceGenerator);
+BENCHMARK(BM_longestIncreasingSubsequence_subsequenceGenerator);
+
+
+const LongestIncreasingSubsequence::Sequence sequenceAlgorithms = {7, 2, 2, 9, 9, 1, 7, 10, 7, 1, 8, 7, 7, 3, 9};
+
+static void BM_longestIncreasingSubsequence_naive(benchmark::State& state) {
+  for (auto _ : state) {
+      const LongestIncreasingSubsequence::NaiveAlgorithm solver(sequenceAlgorithms);
+  }
+}
+BENCHMARK(BM_longestIncreasingSubsequence_naive);
 
 BENCHMARK_MAIN();
