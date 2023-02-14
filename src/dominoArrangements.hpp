18a5aa3bf2de65b15b562dcb84ba527acb930008
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-02-14 08:08:15 -0300

    feat(domino arrangements): implement recursive solver

--- src/dominoArrangements.hpp
+++ src/dominoArrangements.hpp
@@ -61,7 +61,8 @@ namespace dominoArrangements {
             Index getSize() const { return this->size; };
             void addDomino(const Domino & domino) {
                 for (const auto & position : domino.getOccupiedPositions()) {
-                    if (this->positionOccupied[position.row][position.column]) {;
+                    this->checkPosition(position);
+                    if (this->positionOccupied[position.row][position.column]) {
                         throw std::runtime_error("position " + std::string(position) + " already occupied");
                     }
                 }
@@ -70,6 +71,28 @@ namespace dominoArrangements {
                     this->positionOccupied[position.row][position.column] = true;
                 }
                 return;
+            }
+            void addDominoList(const std::vector<Domino> list) {
+                for (const auto & d : list) {
+                    this->addDomino(d);
+                }
+                return;
+            }
+            void checkPosition(const Position & position) {
+                if (position.column >= this->size) {
+                    throw std::runtime_error("invalid column position. Got " + std::to_string(position.column) + " but expected at most " + std::to_string(this->size-1));
+                }
+                if (position.row >= this->rowsLimit) {
+                    throw std::runtime_error("invalid row position. Got " + std::to_string(position.row) + " but expected at most " + std::to_string(this->rowsLimit-1));
+                }
+                return;
+            }
+            static char getNewChar() {
+                static size_t i = 0;
+                static std::array characterArray = {'a', 'b', 'c', 'd', 'e'};
+                const auto ret = characterArray[i];
+                i = (i  + 1) % characterArray.size();
+                return ret;
             }
             operator std::string() const {
                 std::string s;
@@ -87,10 +110,20 @@ namespace dominoArrangements {
                 s += "-";
                 s += "\n";
                 s += "|";
+                std::array<std::vector<char>,2> charMatrix;
+                for (auto & m : charMatrix) {
+                    m.resize(this->size);
+                    std::fill(m.begin(), m.end(), ' ');
+                }
+                for (const auto & d : this->pieces) {
+                    const auto c = getNewChar();
+                    for (const auto & p : d.getOccupiedPositions()) {
+                        charMatrix[p.row][p.column] = c;
+                    }
+                }
                 r = 0;
                 for (std::size_t c = 0 ; c < this->size; ++c) {
-                    if (this->positionOccupied[r][c]) s += "x";
-                    else s += " ";
+                    s += charMatrix[r][c];
                     s += "|";
                 }
                 s += "\n";
@@ -102,8 +135,7 @@ namespace dominoArrangements {
                 s += "|";
                 r = 1;
                 for (std::size_t c = 0 ; c < this->size; ++c) {
-                    if (this->positionOccupied[r][c]) s += "x";
-                    else s += " ";
+                    s += charMatrix[r][c];
                     s += "|";
                 }
                 s += "\n";
@@ -114,22 +146,102 @@ namespace dominoArrangements {
                 return s;
             };
         private:
+            const Index rowsLimit = 2;
             const Index size;
             std::vector<Domino> pieces;
             std::array<std::vector<bool>, 2> positionOccupied;
     };
 
-    // class RecursiveAlgorithm {
-    //     public:
-    //         RecursiveAlgorithm(const Index size)
-    //         : size(size) {}
+    using DominoVector = std::vector<Domino>;
+    using DominoMatrix = std::vector<std::vector<Domino>>;
 
-    //         bool isValid() {
+    class RecursiveAlgorithm {
+        public:
+            const Index size;
+            std::vector<Grid> grids;
 
-    //         }
-    //     private:
-    //         const Index size;
-    // };
+            RecursiveAlgorithm(const Index size)
+                : size(size) {
+                const auto allSolutions = this->solutionRecursion(this->size);
+                for (const auto & solution : allSolutions) {
+                    Grid grid(this->size);
+                    grid.addDominoList(solution);
+                    this->grids.push_back(grid);
+                }
+                return;
+            }
+            Index getNumberOfSolutions() {
+                return this->grids.size();
+            }
+            DominoVector oneVertical(const Index column) {
+                return {
+                    {{0, column}, Orientation::VERTICAL}
+                };
+            }
+            DominoVector twoVertical(const Index column) {
+                return {
+                    {{0, column}, Orientation::VERTICAL},
+                    {{0, column+1}, Orientation::VERTICAL}
+                };
+            }
+            DominoVector twoHorizontal(const Index column) {
+                return {
+                    {{0, column}, Orientation::HORIZONTAL},
+                    {{1, column}, Orientation::HORIZONTAL}
+                };
+            }
+            DominoMatrix solutionRecursion(const Index size) {
+                if (size == 1) {
+                    return { this->oneVertical(size-1) };
+                } else if (size == 2) {
+                    return {
+                        twoHorizontal(size-2),
+                        twoVertical(size-2)
+                    };
+                } else {
+                    DominoMatrix solutions;
+                    // 1
+                    const auto x1 = this->join(
+                        this->solutionRecursion(size-1),
+                        oneVertical(size-1)
+                    );
+                    for (const auto & x : x1) {
+                        solutions.push_back(x);
+                    }
+                    // 2
+                    const auto x2 = this->join(
+                        this->solutionRecursion(size-2),
+                        twoHorizontal(size-2)
+                    );
+                    for (const auto & x : x2) {
+                        solutions.push_back(x);
+                    }
+                    return solutions;
+                }
+            }
+            DominoMatrix join(const DominoMatrix & lhs, const DominoVector & rhs) {
+                DominoMatrix result;
+                for (const auto & l : lhs) {
+                    auto temp = l;
+                    for (const auto & r : rhs) {
+                        temp.push_back(r);
+                    }
+                    result.push_back(temp);
+                }
+                return result;
+            }
+            operator std::string() const {
+                std::string s;
+                s += "Solutions for N = " + std::to_string(this->size) + "\n";
+                s += "Number of solutions = " + std::to_string(this->grids.size()) + "\n\n";
+                for (Index i = 0; i < this->grids.size(); ++i) {
+                    const auto & grid = this->grids[i];
+                    s += "Grid " + std::to_string(i) + "\n";
+                    s += std::string(grid) + "\n\n";
+                }
+                return s;
+            }
+    };
 
 }
 
--- test/dominoArrangements.test.cpp
+++ test/dominoArrangements.test.cpp
@@ -1,20 +1,48 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
+#include <vector>
 
 #include "dominoArrangements.hpp"
 
 using namespace dominoArrangements;
-using std::cout, std::endl;
+// using std::cout, std::endl;
 
-TEST_CASE("basic cases", "[dominoArrangements]") {
+TEST_CASE("add domino to grid", "[dominoArrangements]") {
     Grid grid(5);
-    grid.addDomino({
-        {0, 0}, Orientation::HORIZONTAL
+    // cout << "===== basic cases =====" << endl;
+    grid.addDominoList({
+        {{0, 0}, Orientation::HORIZONTAL},
+        {{0, 3}, Orientation::VERTICAL}
     });
-    grid.addDomino(
-        {{0, 0}, Orientation::VERTICAL}
-    );
-    cout << std::string(grid) << endl;
+    // cout << std::string(grid) << endl;
+    // cout << "==========" << endl << endl;
 }
+
+struct NumberOfSolutionsTestCase {
+    const Index size;
+    const Index expected;
+};
+
+TEST_CASE("number of solutions", "[dominoArrangements]") {
+    std::vector<NumberOfSolutionsTestCase> testCaseList = {
+        {1, 1},
+        {2, 2},
+        {3, 3},
+        {4, 5},
+        {5, 8},
+        {6, 13}
+    };
+    for (const auto & testCase : testCaseList) {
+        RecursiveAlgorithm solver(testCase.size);
+        CHECK(solver.getNumberOfSolutions() == testCase.expected);
+    }
+}
+
+// TEST_CASE("print solutions", "[dominoArrangements]") {
+//     cout << "===== recursive solution =====" << endl;
+//     RecursiveAlgorithm solver(4);
+//     cout << std::string(solver);
+//     cout << "==========" << endl << endl;
+// }
