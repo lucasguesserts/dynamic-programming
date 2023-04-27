e2ecf7fd1202adca92bd259144c0b18f130ac398
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-04-27 17:51:49 -0300

    test(knapsack): refactor: improve the code

--- test/knapsack.test.cpp
+++ test/knapsack.test.cpp
@@ -18,90 +18,57 @@ TEST_CASE("instance", "[knapsack]") {
     }
 }
 
-TEST_CASE("naive", "[knapsack]") {
-    SECTION("case 1") {
-        auto values = ValueVector{60, 100, 120};
-        auto weights = WeightVector{10, 20, 30};
-        auto capacity = Weight{50};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<Naive>(instance));
-        auto expected_value = Value{220};
-        auto expected_weight = Weight{50};
-        auto expected_items = ItemSet{1, 2};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
+struct Expected {
+    Value value{0};
+    Weight weight{0};
+    ItemSet items{};
+};
+
+template <typename AlgorithmType>
+void test_algorithm(const Instance & instance, const Expected & expected) {
+    auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<AlgorithmType>(instance));
+    REQUIRE(algorithm->get_value() == expected.value);
+    REQUIRE(algorithm->get_weight() == expected.weight);
+    REQUIRE(algorithm->get_items() == expected.items);
+}
+
+TEST_CASE("case 1", "[knapsack]") {
+    const auto values = ValueVector{60, 100, 120};
+    const auto weights = WeightVector{10, 20, 30};
+    const auto capacity = Weight{50};
+    const auto instance = Instance{values, weights, capacity};
+    const auto expected_value = Value{220};
+    const auto expected_weight = Weight{50};
+    const auto expected_items = ItemSet{1, 2};
+    const auto expected = Expected{expected_value, expected_weight, expected_items};
+    SECTION("naive") {
+        test_algorithm<Naive>(instance, expected);
     }
-    SECTION("case 2") {
-        auto values = ValueVector{360, 83, 59, 130, 431};
-        auto weights = WeightVector{7, 0, 30, 22, 80};
-        auto capacity = Weight{52};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<Naive>(instance));
-        auto expected_value = Value{573};
-        auto expected_weight = Weight{29};
-        auto expected_items = ItemSet{0, 1, 3};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
+    SECTION("recursion") {
+        test_algorithm<Recursion>(instance, expected);
+    }
+    SECTION("dynamic programming") {
+        test_algorithm<DynamicProgramming>(instance, expected);
     }
 }
 
-TEST_CASE("recursion", "[knapsack]") {
-    SECTION("case 1") {
-        auto values = ValueVector{60, 100, 120};
-        auto weights = WeightVector{10, 20, 30};
-        auto capacity = Weight{50};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<Recursion>(instance));
-        auto expected_value = Value{220};
-        auto expected_weight = Weight{50};
-        auto expected_items = ItemSet{1, 2};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
+TEST_CASE("case 2", "[knapsack]") {
+    const auto values = ValueVector{360, 83, 59, 130, 431};
+    const auto weights = WeightVector{7, 0, 30, 22, 80};
+    const auto capacity = Weight{52};
+    const auto instance = Instance{values, weights, capacity};
+    const auto expected_value = Value{573};
+    const auto expected_weight = Weight{29};
+    const auto expected_items = ItemSet{0, 1, 3};
+    const auto expected = Expected{expected_value, expected_weight, expected_items};
+    SECTION("naive") {
+        test_algorithm<Naive>(instance, expected);
     }
-    SECTION("case 2") {
-        auto values = ValueVector{360, 83, 59, 130, 431};
-        auto weights = WeightVector{7, 0, 30, 22, 80};
-        auto capacity = Weight{52};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<Recursion>(instance));
-        auto expected_value = Value{573};
-        auto expected_weight = Weight{29};
-        auto expected_items = ItemSet{0, 1, 3};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
+    SECTION("recursion") {
+        test_algorithm<Recursion>(instance, expected);
     }
-}
-
-TEST_CASE("dynamic programming", "[knapsack]") {
-    SECTION("case 1") {
-        auto values = ValueVector{60, 100, 120};
-        auto weights = WeightVector{10, 20, 30};
-        auto capacity = Weight{50};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<DynamicProgramming>(instance));
-        auto expected_value = Value{220};
-        auto expected_weight = Weight{50};
-        auto expected_items = ItemSet{1, 2};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
-    }
-    SECTION("case 2") {
-        auto values = ValueVector{360, 83, 59, 130, 431};
-        auto weights = WeightVector{7, 0, 30, 22, 80};
-        auto capacity = Weight{52};
-        auto instance = Instance{values, weights, capacity};
-        auto algorithm = std::unique_ptr<Algorithm>(std::make_unique<DynamicProgramming>(instance));
-        auto expected_value = Value{573};
-        auto expected_weight = Weight{29};
-        auto expected_items = ItemSet{0, 1, 3};
-        REQUIRE(algorithm->get_value() == expected_value);
-        REQUIRE(algorithm->get_weight() == expected_weight);
-        REQUIRE(algorithm->get_items() == expected_items);
+    SECTION("dynamic programming") {
+        test_algorithm<DynamicProgramming>(instance, expected);
     }
 }
 
